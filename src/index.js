bca24525aa8fc3cebd81cc15daee3e724f562449
Georges-Antoine Assi <GAntoine@users.noreply.github.com>
2016-08-14 14:18:09 -0400

    feat: Purged TheGamesDB, added NES games (#2)
    TheGamesDB was not a needed dependency, removed to keep the scope of the library small enough. Added
    a list of NES games. Refactored function returns. Added the platform API.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,43 @@
 import uniqueRandomArray from 'unique-random-array'
-import retroNames from './retro-game-names.json'
+import platforms from './data/platforms.js'
 import _ from 'underscore'
-import Game from './game.js'
-
-const getRandomItem = uniqueRandomArray(retroNames)
 
 const mainExport = {
-  all: retroNames,
+  all: platforms,
+  platform: platform,
   random: random,
   find: find
 };
 
-function random(arg) {
-  if (arg === undefined) {
-    return new Game(getRandomItem())
-  } else if (typeof arg === 'string') {
-    const foundNames = _.filter(retroNames, (s) => s.includes(arg))
-    return new Game(uniqueRandomArray(foundNames)())
+function platform(console) {
+  return platforms[console]
+}
+
+// Returns an object of type {title, platform}
+function random(options = {}) {
+  const _platform = options.platform || _randomPlatform()
+  const game = uniqueRandomArray(platforms[_platform])()
+  return {title: game, platform: _platform}
+}
+
+// Returns on object of type {platform, [titles]}
+function find(options = {}) {
+  if (_.isEmpty(options) || !options.title) {
+    return 'Error: You must pass options containing a title (required).'
+  } else if (options.platform) {
+    const names = _.filter(platforms[options.platform], (s) => s.includes(options.title))
+    return {platform: options.platform, titles: names}
   } else {
-    const randomItems = [];
-    for (let i = 0; i < arg; i++) {
-      randomItems.push(new Game(getRandomItem()))
-    }
-    return randomItems
+    return _.mapObject(platforms, (games) => {
+      return _.filter(games, (s) => s.includes(options.title))
+    })
   }
 }
 
-function find(name, number) {
-  const foundNames = _.filter(retroNames, (s) => s.includes(name))
-
-  const names = []
-  for (let i = 0; i < foundNames.length; i++){
-    names.push(new Game(foundNames[i]))
-  }
-
-  if (number === undefined) {
-    return names
-  } else {
-    return _.shuffle(names).slice(0, number)
-  }
+function _randomPlatform() {
+  const keys = Object.keys(platforms)
+  return uniqueRandomArray(keys)()
 }
-
-
 
 export default mainExport
 module.exports = mainExport // for CommonJS compatibility
--- src/index.test.js
+++ src/index.test.js
@@ -1,54 +1,63 @@
 import {expect} from 'chai'
 import retroNames from '.'
-import _ from 'underscore'
 
 describe('retro-game-names', () => {
   describe('all', () => {
-    it('should return an array of strings', () => {
-      expect(retroNames.all).to.satisfy(isArrayOfStrings)
+    it('should return an object of platforms', () => {
+      const all = retroNames.all
+
+      expect(all).to.be.an('object')
+      expect(all).to.have.all.keys(['snes', 'nes'])
     })
 
     it('should contain the game `Casper`', () => {
-      expect(retroNames.all).to.include('Casper')
+      expect(retroNames.all.snes).to.include('Casper')
+    })
+  })
+
+  describe('platform', () => {
+    it('should return an array of games for the given plarform', () => {
+      const games = retroNames.platform('snes')
+
+      expect(games).to.be.instanceof(Array)
+      expect(games).to.include('Super Mario World')
     })
   })
 
   describe('random', () => {
-    it('should return a random item from retroNames.all', () => {
+    it('should return a random {title, platform} from retroNames.all', () => {
       const randomItem = retroNames.random()
-      expect(retroNames.all).to.include(randomItem.title)
-    });
 
-    it('should return an array of random items if passed a number', () => {
-      const randomItems = retroNames.random(3)
-      expect(randomItems).to.have.length(3)
-      randomItems.forEach(function(item) {
-        expect(retroNames.all).to.include(item.title)
-      });
-    });
+      expect(retroNames.all[randomItem.platform]).to.include(randomItem.title)
+    })
 
-    it('should return a random item containing a given string', () => {
-      const randomName = retroNames.random('Aero')
-      expect(retroNames.all).to.include(randomName.title)
-    });
+    it('should return a random {title, platform} when given a platform', () => {
+      const randomItem = retroNames.random({platform: 'snes'})
+
+      expect(retroNames.all.snes).to.include(randomItem.title)
+    })
   })
 
   describe('find', () => {
-    it('should return an array of names containing a given string', () => {
-      const foundNames = retroNames.find('Aero')
-      expect(_.pluck(foundNames, 'title')).to.include('Aero Fighters')
-    });
+    it('should return an object with multiple {platform, [titles]}', () => {
+      const foundNames = retroNames.find({title: 'Aero'})
 
-    it('should return an array of names of a maximum given length', () => {
-      const foundNames = retroNames.find('Battle', 5)
-      expect(foundNames).to.have.length.below(6)
-      foundNames.forEach(function(item) {
-        expect(retroNames.all).to.include(item.title)
-      });
-    });
+      expect(foundNames.snes).to.include('Aero Fighters')
+    })
+
+    it('should return a {platform, [titles]} when given a platform', () => {
+      const foundNames = retroNames.find({platform: 'snes', title: 'Battle'})
+
+      expect(foundNames.platform).to.equal('snes')
+      foundNames.titles.forEach(function(item) {
+        expect(retroNames.all.snes).to.include(item)
+      })
+    })
+
+    it('should return an error string if a title is not passed', () => {
+      const foundNames = retroNames.find()
+
+      expect(foundNames).to.have.string('Error:')
+    })
   })
 })
-
-function isArrayOfStrings(array) {
-  return array.every(i => typeof i === 'string')
-}
